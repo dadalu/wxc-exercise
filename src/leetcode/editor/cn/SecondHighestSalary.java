0ad513da7838b29f00e6b2c3b143bb426a15576c
wangxiaocheng <wangxiaocheng@linewell.com>
2020-05-12 10:28:11 +0800

    刷到二进制算法题

---
+++ src/leetcode/editor/cn/SecondHighestSalary.java
@@ -0,0 +1,30 @@
+//编写一个 SQL 查询，获取 Employee 表中第二高的薪水（Salary） 。 
+//
+// +----+--------+
+//| Id | Salary |
+//+----+--------+
+//| 1  | 100    |
+//| 2  | 200    |
+//| 3  | 300    |
+//+----+--------+
+// 
+//
+// 例如上述 Employee 表，SQL查询应该返回 200 作为第二高的薪水。如果不存在第二高的薪水，那么查询应返回 null。 
+//
+// +---------------------+
+//| SecondHighestSalary |
+//+---------------------+
+//| 200                 |
+//+---------------------+
+// 
+//
+
+/*package leetcode.editor.cn;
+public class  SecondHighestSalary{
+    public static void main(String[] args){
+        Solution solution = new SecondHighestSalary().new Solution();
+    }
+    
+//There is no code of Java type for this problem
+
+}*/
