cc9242ceaa7c647fcd60f01444e8736d496164f6
wangxiaocheng <xiaocheng.wang@china317.com>
2020-09-30 13:47:46 +0800

    重塑矩阵

---
+++ src/leetcode/editor/cn/ReshapeTheMatrix.java
@@ -0,0 +1,80 @@
+package leetcode.editor.cn;
+//重塑矩阵
+//在MATLAB中，有一个非常有用的函数 reshape，它可以将一个矩阵重塑为另一个大小不同的新矩阵，但保留其原始数据。 
+//
+// 给出一个由二维数组表示的矩阵，以及两个正整数r和c，分别表示想要的重构的矩阵的行数和列数。 
+//
+// 重构后的矩阵需要将原始矩阵的所有元素以相同的行遍历顺序填充。 
+//
+// 如果具有给定参数的reshape操作是可行且合理的，则输出新的重塑矩阵；否则，输出原始矩阵。 
+//
+// 示例 1: 
+//
+// 
+//输入: 
+//nums = 
+//[[1,2],
+// [3,4]]
+//r = 1, c = 4
+//输出: 
+//[[1,2,3,4]]
+//解释:
+//行遍历nums的结果是 [1,2,3,4]。新的矩阵是 1 * 4 矩阵, 用之前的元素值一行一行填充新矩阵。
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: 
+//nums = 
+//[[1,2],
+// [3,4]]
+//r = 2, c = 4
+//输出: 
+//[[1,2],
+// [3,4]]
+//解释:
+//没有办法将 2 * 2 矩阵转化为 2 * 4 矩阵。 所以输出原矩阵。
+// 
+//
+// 注意： 
+//
+// 
+// 给定矩阵的宽和高范围在 [1, 100]。 
+// 给定的 r 和 c 都是正数。 
+// 
+// Related Topics 数组 
+// 👍 138 👎 0
+
+public class ReshapeTheMatrix{
+    public static void main(String[] args) {
+        Solution solution = new ReshapeTheMatrix().new Solution();
+        int[][] arr = {{1,2},{3,4}};
+        solution.matrixReshape(arr,4,2);
+        
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[][] matrixReshape(int[][] nums, int r, int c) {
+        if(nums==null||nums.length==0||nums.length*nums[0].length<r*c)return nums;
+        int r1 = 0;
+        int c1 = 0;
+        int[][] retNums = new int[r][c];
+        for (int i = 0; i < r; i++) {
+            int[] num = new int[c];
+            for (int j = 0; j < c; j++) {
+                num[j]=nums[r1][c1];
+                if(++c1==nums[0].length){
+                    r1++;
+                    c1=0;
+                }
+            }
+            retNums[i]=num;
+        }
+        return retNums;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
