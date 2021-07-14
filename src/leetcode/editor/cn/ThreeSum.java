d8492a138ff22ccb213413816ae05df6e6d18c0f
wangxiaocheng <xiaocheng.wang@china317.com>
2021-07-14 13:44:14 +0800

    三数之和

--- src/leetcode/editor/cn/ThreeSum.java
+++ src/leetcode/editor/cn/ThreeSum.java
@@ -19,17 +19,53 @@
 
 package leetcode.editor.cn;
 
-import java.util.List;
+import java.util.*;
 
 public class  ThreeSum{
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Solution solution = new ThreeSum().new Solution();
+        int[] arr = {-2, 0, 0,2,2};
+        solution.threeSum(arr);
     }
     
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-return  null;
+        Arrays.sort(nums);
+        List<List<Integer>> list = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i]>0) {
+                break;
+            }
+            if(i>0&&nums[i]==nums[i-1]) {
+                continue;
+            }
+            int L =i + 1;
+            int R =nums.length - 1;
+
+            while(L < R){
+                int sum = nums[i]+nums[L]+nums[R];
+                if(sum == 0){
+                    list.add(Arrays.asList(nums[i],nums[L],nums[R]));
+                    while(L<R&&nums[L]==nums[L+1]) {
+                        L++;
+                    }
+                    while(L<R&&nums[R]==nums[R-1]) {
+                        R--;
+                    }
+                    L++;
+                    R--;
+                }
+                else{
+                    if(sum<0) {
+                        L++;
+                    } else {
+                        R--;
+                    }
+                }
+            }
+        }
+        return list;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
