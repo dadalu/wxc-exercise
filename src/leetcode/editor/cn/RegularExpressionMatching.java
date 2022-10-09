a54b1ff6b28b9041769b8af1906044b35981e7d9
wangxiaocheng <wang_xiao_cheng@sina.com>
2022-10-09 15:40:36 +0800

    正则表达式匹配

--- src/leetcode/editor/cn/RegularExpressionMatching.java
+++ src/leetcode/editor/cn/RegularExpressionMatching.java
@@ -1,76 +1,88 @@
-/*
-//给你一个字符串 s 和一个字符规律 p，请你来实现一个支持 '.' 和 '*' 的正则表达式匹配。
+package leetcode.editor.cn;
+//正则表达式匹配
+//给你一个字符串 s 和一个字符规律 p，请你来实现一个支持 '.' 和 '*' 的正则表达式匹配。 
 //
-// '.' 匹配任意单个字符
-//'*' 匹配零个或多个前面的那一个元素
+// 
+// '.' 匹配任意单个字符 
+// '*' 匹配零个或多个前面的那一个元素 
 // 
 //
 // 所谓匹配，是要涵盖 整个 字符串 s的，而不是部分字符串。 
 //
-// 说明: 
+// 示例 1： 
 //
 // 
-// s 可能为空，且只包含从 a-z 的小写字母。 
-// p 可能为空，且只包含从 a-z 的小写字母，以及字符 . 和 *。 
-// 
-//
-// 示例 1: 
-//
-// 输入:
-//s = "aa"
-//p = "a"
-//输出: false
-//解释: "a" 无法匹配 "aa" 整个字符串。
+//输入：s = "aa", p = "a"
+//输出：false
+//解释："a" 无法匹配 "aa" 整个字符串。
 // 
 //
 // 示例 2: 
 //
-// 输入:
-//s = "aa"
-//p = "a*"
-//输出: true
-//解释: 因为 '*' 代表可以匹配零个或多个前面的那一个元素, 在这里前面的元素就是 'a'。因此，字符串 "aa" 可被视为 'a' 重复了一次。
+// 
+//输入：s = "aa", p = "a*"
+//输出：true
+//解释：因为 '*' 代表可以匹配零个或多个前面的那一个元素, 在这里前面的元素就是 'a'。因此，字符串 "aa" 可被视为 'a' 重复了一次。
 // 
 //
-// 示例 3: 
+// 示例 3： 
 //
-// 输入:
-//s = "ab"
-//p = ".*"
-//输出: true
-//解释: ".*" 表示可匹配零个或多个（'*'）任意字符（'.'）。
+// 
+//输入：s = "ab", p = ".*"
+//输出：true
+//解释：".*" 表示可匹配零个或多个（'*'）任意字符（'.'）。
 // 
 //
-// 示例 4: 
-//
-// 输入:
-//s = "aab"
-//p = "c*a*b"
-//输出: true
-//解释: 因为 '*' 表示零个或多个，这里 'c' 为 0 个, 'a' 被重复一次。因此可以匹配字符串 "aab"。
 // 
 //
-// 示例 5: 
+// 提示： 
 //
-// 输入:
-//s = "mississippi"
-//p = "mis*is*p*."
-//输出: false 
-// Related Topics 字符串 动态规划 回溯算法
+// 
+// 1 <= s.length <= 20 
+// 1 <= p.length <= 30 
+// s 只包含从 a-z 的小写字母。 
+// p 只包含从 a-z 的小写字母，以及字符 . 和 *。 
+// 保证每次出现字符 * 时，前面都匹配到有效的字符 
+// 
+//
+// 👍 3244 👎 0
 
-package leetcode.editor.cn;
-public class  RegularExpressionMatching{
-    public static void main(String[] args){
+public class RegularExpressionMatching{
+    public static void main(String[] args) {
         Solution solution = new RegularExpressionMatching().new Solution();
+        boolean result = solution.isMatch("aa","a*");
+        System.out.println(result);
     }
-    
+
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public boolean isMatch(String s, String p) {
-
+        int sLen = s.length();
+        int pLen = p.length();
+        boolean[][] dp = new boolean[sLen+1][pLen+1];
+        dp[0][0] = true;
+        for (int i = 1; i < pLen+1; i++) {
+            if (p.charAt(i - 1) == '*') {
+                dp[0][i] = dp[0][i - 2];
+            }
+        }
+        for (int i = 1; i < sLen+1; i++) {
+            for (int j = 1; j < pLen+1; j++) {
+                if (p.charAt(j-1) == '*') {
+                    dp[i][j] = dp[i][j - 2];
+                    if (s.charAt(i-1) == p.charAt(j - 2) || p.charAt(j-2) == '.') {
+                        dp[i][j] = dp[i][j] || dp[i-1][j-2]||dp[i-1][j];
+                    }
+                } else {
+                    if (s.charAt(i-1) == p.charAt(j-1)||p.charAt(j-1)=='.') {
+                        dp[i][j] = dp[i-1][j-1];
+                    }
+                }
+            }
+        }
+        return dp[sLen][pLen];
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
-*/
