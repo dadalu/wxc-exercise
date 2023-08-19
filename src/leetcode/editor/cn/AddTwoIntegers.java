6cb525de6b656ab31e6bccddc7de15f882a8a7dd
wangxiaocheng <wang_xiao_cheng@sina.com>
2023-08-19 18:26:38 +0800

    两整数相加

---
+++ src/leetcode/editor/cn/AddTwoIntegers.java
@@ -0,0 +1,49 @@
+package leetcode.editor.cn;
+//两整数相加
+//add-two-integers	
+//2235
+//给你两个整数 num1 和 num2，返回这两个整数的和。
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：num1 = 12, num2 = 5
+//输出：17
+//解释：num1 是 12，num2 是 5 ，它们的和是 12 + 5 = 17 ，因此返回 17 。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：num1 = -10, num2 = 4
+//输出：-6
+//解释：num1 + num2 = -6 ，因此返回 -6 。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// -100 <= num1, num2 <= 100 
+// 
+//
+// 👍 182 👎 0
+
+//Java：两整数相加
+public class AddTwoIntegers{
+    public static void main(String[] args) {
+        Solution solution = new AddTwoIntegers().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
